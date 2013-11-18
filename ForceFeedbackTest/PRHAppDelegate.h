f925c009a5f21902d2ede7d2cfcf04ab41a2d3fa
Peter Hosey <git@boredzo.org>
2013-11-17 21:24:21 -0800

    Initial Commit

---
+++ ForceFeedbackTest/PRHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRHAppDelegate.h
+//  ForceFeedbackTest
+//
+//  Created by Peter Hosey on 2013-11-17.
+//  Copyright (c) 2013 Peter Hosey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PRHAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ForceFeedbackTest/PRHAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  PRHAppDelegate.m
+//  ForceFeedbackTest
+//
+//  Created by Peter Hosey on 2013-11-17.
+//  Copyright (c) 2013 Peter Hosey. All rights reserved.
+//
+
+#import "PRHAppDelegate.h"
+
+@implementation PRHAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
